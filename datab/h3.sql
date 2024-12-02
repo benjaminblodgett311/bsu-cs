53f8839cb5fe5339303709c78283d9d95a51441f
ben256dev <benjamin@ben256.com>
2024-12-02 03:49:20 +0000

    create statements

--- datab/h3.sql
+++
@@ -1,9 +0,0 @@
-create table bookmark (
-   varchar(1024) url;
-   boolean is_private;
-   timestamp create_date;
-   timestamp update_date;
-   text description;
-   varchar(64) title;
-   primary key (bookmark_id);
-);
--- datab/hw3.sql
+++ datab/hw3.sql
@@ -2,7 +2,7 @@ create database if not exists hw3;
 
 use hw3;
 
-create table bookmark (
+create table if not exists bookmark (
 	url varchar(1024) not null,
 	is_private boolean not null,
 	create_date timestamp not null,
@@ -12,21 +12,22 @@ create table bookmark (
 	bookmark_id int auto_increment primary key
 );
 
-create table tag (
+create table if not exists tag (
    tagname varchar(64) not null,
    bookmark_id int not null,
-   primary key (tagname, bookmark_id)
+   primary key (tagname, bookmark_id),
    foreign key (bookmark_id) references bookmark(bookmark_id)
 );
 
-create table site_cache (
-   url varchar(1024) primary key,
+create table if not exists site_cache (
+   cache_id int primary key,
+   url varchar(2048),
    cache_date timestamp not null,
    success boolean not null,
    html text
 );
 
-create table userperson (
+create table if not exists userperson (
    create_date timestamp not null,
    username varchar(32) not null,
    password_hash char(64) not null,
@@ -34,8 +35,8 @@ create table userperson (
    user_id int auto_increment primary key
 );
 
-create table subscription (
-   other_user_id int auto_increment primary key,
-   user_id int auto_increment,
+create table if not exists subscription (
+   subscriber_user_id int auto_increment primary key,
+   user_id int,
    foreign key (user_id) references userperson(user_id)
 );
